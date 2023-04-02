ac3cadc3e19ffb0a70aff73e2d6c8b357d89c670
Mikel Cromphoudt <mikel.cromphoudt@gmail.com>
2023-04-02 15:58:03 +0200

    nesting des routes avec Layout

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Layout from './pages/layout/Layout'
 import Home from './pages/home/Home'
 import Housing from './pages/housing/Housing'
 import About from './pages/about/About'
 import Error404 from './pages/error404/Error404'
+import './index.scss'
 
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
@@ -13,10 +15,12 @@ root.render(
     <React.StrictMode>
         <Router>
             <Routes>
-                <Route exact path="/" element={<Home />} />
-                  <Route path="/housing" element={< Housing/>} />
-                  <Route path="/about" element={< About/>} />
-                  <Route path="/*" element={< Error404/>} />
+                <Route exact path="/" element={<Layout />}>
+                    <Route index element={< Home/>} />
+                    <Route path="housing" element={< Housing/>} />
+                    <Route path="about" element={< About/>} />
+                    <Route path="*" element={< Error404/>} />
+                </Route>
             </Routes>
         </Router>
     </React.StrictMode>
