4107c6401d836a68bf929097e69e106304e6379e
Mikel Cromphoudt <mikel.cromphoudt@gmail.com>
2023-04-03 01:11:17 +0200

    import sass global variables okay

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,22 @@
 import {Link} from 'react-router-dom'
 import './Header.scss'
+import Logo from '../../assets/logo.png'
 
 const Header = () => {
     return(
-    <nav>
-        <ul className='menu'>
-          <li className='menu-item'>
-            <Link to="/">Accueil</Link>
-          </li>
-          <li className='menu-item'>
-            <Link to="/about">A propos</Link>
-          </li>
-        </ul>
-      </nav>
+        <div className='header'>
+            <p className='logo-title'>K<img src={Logo} className='style-logo' alt='logo-Kasa'/>sa</p>
+            <nav>
+                <ul className='menu'>
+                <li className='menu-item'>
+                    <Link to="/">Accueil</Link>
+                </li>
+                <li className='menu-item'>
+                    <Link to="/about">A propos</Link>
+                </li>
+                </ul>
+            </nav>
+        </div>
       );
 };
 
