305fd21e5a3436a5f4cf95996820038ebe05657d
Mikel Cromphoudt <mikel.cromphoudt@gmail.com>
2023-04-03 18:26:32 +0200

    ajout de state et effect dans main

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from 'react';
 import './Home.scss';
 
 function Home() {
+  //Pour utiliser les states, il faut toujours définir un 'getter' et un 'setter'
+  const [logements, setLogements] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('./data.json')
+    .then((response) => response.json)
+    .then((data) => {
+      setLogements(data);
+      setIsLoading(false);
+    });
+  }, []); //les crochets vides sont en fait le tableau de dépendance pour le refresh de la section concernée
+
   return (
     <div className='main'>Main Page</div>
   );
