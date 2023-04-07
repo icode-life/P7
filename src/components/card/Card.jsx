e7251e6e4c32509ab8de1452eb25fd36ef53b9bb
Mikel Cromphoudt <mikel.cromphoudt@gmail.com>
2023-04-07 17:57:38 +0200

    card images centered

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,7 +4,7 @@ import './Card.scss'
 const Card = ({title, cover}) => {
     return(
         <Link to="/housing">
-            <div style={{background: `linear-gradient(to bottom, transparent 0%, transparent 80%, #555555 100%), url(${cover})`}} className='card'>
+            <div style={{backgroundImage: `linear-gradient(to bottom, transparent 0%, transparent 80%, #555555 100%), url(${cover})`, backgroundPosition: "center"}} className='card'>
                 <p className='card-title'>{title}</p>
             </div>
         </Link>
