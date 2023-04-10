e9977acef1d67a3fcb0b7696daadc3076d4d9ab1
Mikel Cromphoudt <mikel.cromphoudt@gmail.com>
2023-04-10 17:29:09 +0200

    page listing import json et mise en page suite

--- src/pages/listing/Listing.jsx
+++ src/pages/listing/Listing.jsx
@@ -20,16 +20,21 @@ function Listing() {
             <div className='listingTitleNProp'>
                 <div className='titleNSub'>
                     <div className='titleMain'>{listingDetails.title}</div>
+                    <div className='sub'>{listingDetails.location}</div>
                 </div>
                 <div className='proprio'>
-
+                    <div className='name'>{listingDetails.host.name}</div>
+                    <img className='pic' src={listingDetails.host.picture}></img>
                 </div>
             </div>
             <div className='tagsNStars'>
                 <div className='tags'>tags</div>
                 <div className='stars'>stars</div>
             </div>
-            <div className='collapses'></div>
+            <div className='collapses'>
+                <div className='collapse'>collapse 1</div>
+                <div className='collapse'>collapse 2</div>
+            </div>
         </div>
     )
 }
