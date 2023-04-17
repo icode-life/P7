c8b9c8e2d0d34574d807c6006b223def4f0d9dec
Mikel Cromphoudt <mikel.cromphoudt@gmail.com>
2023-04-17 22:59:20 +0200

    carousel in progress

--- src/pages/listing/Listing.jsx
+++ src/pages/listing/Listing.jsx
@@ -1,11 +1,12 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import './Listing.scss';
-import ListingsData from '../../assets/listings.json';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
-/* import { faStar as faStarEmpty } from '@fortawesome/free-regular-svg-icons' */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import Collapsible from '../../components/collapse/Collapse';
+import Error404 from '../error404/Error404';
 
 
 function Listing() {
@@ -20,18 +21,36 @@ function Listing() {
         .then((data) => {
             setListing(data.find(x => x.id === listingId));
             setIsLoading(false);
-            console.log(isLoading);
         });
     }, []);
     
     const rating = [1, 2, 3, 4, 5];
     
+     if (listing === undefined){
+        return <Error404 />
+     } //gestion de traficotage de l'url au niveau des ids en paramètres
+
     return (
         <div>
             {!isLoading ? (
             <div>
                 <div className='carouselContainer'>
-                    <img src={listing.pictures[0]} alt="photos carousel" className='carousel'/>
+                    <div id="images" className='wrapper'>
+                        {listing.pictures.map((pic, index) => {
+                            return (
+                                    <img key={index + 100} src={pic} alt="listing picture" className='carouselPic' />
+                            )       
+                        })}
+                    {/* <img src={listing.pictures[0]} alt="photos carousel" className='carousel'/> */}
+                    </div>
+                    <div className="buttons">
+                        <button className='prev'>
+                            <FontAwesomeIcon icon={faChevronLeft} className='carouselButtons' />
+                        </button>
+                        <button className='next'>
+                            <FontAwesomeIcon icon={faChevronRight} className='carouselButtons' />
+                        </button>
+                    </div>
                 </div>
                 <div className='listingTitleNProp'>
                     <div className='titleNSub'>
